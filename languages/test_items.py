8bf96eff44ad4fc79d730c2b687ac0c4d3b7ce62
BobrovPavel <avel.babrou@startmatter.com>
2019-06-21 11:32:23 +0300

    update test for correct assertion error

--- languages/test_items.py
+++ languages/test_items.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-button_locator = (By.XPATH, "//button[contains(@class,'btn-add-to-basket')]")
+button_locator = (By.XPATH, "ыы//button[contains(@class,'btn-add-to-basket')]")
 
 
 def test_button_availability(browser):
     browser.get(link)
-    button = browser.find_element(*button_locator)
-    assert button.is_displayed() and button.is_enabled(), "The item on the locator [%s] is not available" % button_locator
+    buttons = browser.find_elements(*button_locator)
+    assert len(buttons) > 0 and buttons[0].is_enabled(), "The item on the locator [%s] is not available" % str(button_locator)
